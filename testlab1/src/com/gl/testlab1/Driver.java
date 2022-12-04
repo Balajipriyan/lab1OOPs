aa9c3a8f09e59f8b76874fb905b715124189deaf
Balajipriyan <119772870+Balajipriyan@users.noreply.github.com>
2022-12-04 09:33:06 +0530

    Update Driver.java

--- testlab1/src/com/gl/testlab1/Driver.java
+++ testlab1/src/com/gl/testlab1/Driver.java
@@ -6,7 +6,7 @@ public class Driver {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("Please Enter the department from the following/n");
+System.out.println("Please Enter the department from the following");
 System.out.println("1.Technical");
 System.out.println("2.Admin");
 System.out.println("3.Human Resource");
